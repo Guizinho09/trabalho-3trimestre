6897d166cec6af3427ae054a573c6962c7d4b7db
Guizinho09 <131281898+Guizinho09@users.noreply.github.com>
2024-10-08 12:14:47 -0300

    Update Untitled-1.js

--- Untitled-1.js
+++ Untitled-1.js
@@ -2,19 +2,25 @@ const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 console.log(url);
 
 async function visualizarInformacoesGlobais (){
+    
     const res = await fetch(url);
     const dados = await res.json();
+    const pessoasConectadas = (dados.total_pessoas_conectadas) / 1e9
+    const pessoasMundo = (dados.total_pessoas_mundo)/1e9 
+    const horas = parseInt (dados.tempo_medio)
+    const minutos = math.round ((dados.tempo_medio - horas)* 100)
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container_texto')
     paragrafo.innerHTML = `Você sabia que o mundo tem 
-    ${dados.total_pessoas_mundo} de pessoas e 
-    que aproximadamente ${dados.total_pessoas_conectadas}
-    estão conectadas em algum rede sociale passam em média 
-    ${dados.tempo_medio} horas conectadas`
-    const container = document.getElementyById(´ǵraficos-container`)
-    conteiner.appendChild(paragrafo)
-    console.log(dados.tempo_medio)
-}
+<span>${pessoasMundo}</span> de pessoas e 
+que aproximadamente <span>${pessoasConectadas}</span>
+estão conectadas em alguma rede social e passam em media 
+<span>${horas}</span> horas e <span>${minutos}</span> minutos conectadas.
+<br> Isso significa que aproximadamente ${porcentagemConectada}%
+de pessoas estão conectadas em alguma rede social.`
+    const container = document.getElementById(ǵraficos-container`
+    constainer.appendChild(paragrafo)
+
 visualizarInformacoesGlobais()
 {
     "total_pessoas_conectadas"; 5.04e9,
