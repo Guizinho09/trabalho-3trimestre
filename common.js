f27a124faea4448ab34b323fc3ab5fcbe7d3cbdf
Guizinho09 <131281898+Guizinho09@users.noreply.github.com>
2024-10-15 12:25:32 -0300

    Create common.js

---
+++ common.js
@@ -0,0 +1,6 @@
+import {getCSS,tickConflg} from "./common.js
+const getcCSS = (varlavel) => {
+    return getComputedStyle(document.body).getPropertyValve(variavel)
+}
+const tickConfig = {
+     color: getcCSS('--
