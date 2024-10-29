83973c9309ab68134cc44a911c54998bf0170a0c
Guizinho09 <131281898+Guizinho09@users.noreply.github.com>
2024-10-29 12:25:00 -0300

    Update redesfavoritasmundo.js

--- redesfavoritasmundo.js
+++ redesfavoritasmundo.js
@@ -1,1 +1,27 @@
+import { getcCSS } from"./common";
+const layout = {
+  plot_bgcolor: getCSS('--bg-color'),
+  paper_bgcolor: getCSS('--bg-color),
+  height: 700,
+  title: {
+
+  }
+const grafico = document.createElement ('div')
+         grafico.className = 'grafico'
+         document.getElementByd('graficos-container')
+         .appendchild(grafico)
+         const config = {
+            responsive: true,
+            displayModeBar: false
+        }
+        Plotly.newPlot(grafico,data,layout,confg)
+
+        criarGrafico(data,layout)
+        incluirTexto(` Embora o <span>Instagram</span> ocupe a quarta posição em termos 
+             de número total de usuários entre as redes sociais, destaca-se como 
+             a <span>preferida dos usuários</span>. supera até mesmo o <span>Facebook</span>, a plataforma
+             com mais usuários, sendo terceira opção mais apreciada pelos usuários.<br>Essa preferéncia
+             envidencia a forte conexão e apreço que as pessoas têm pelo Instagram em comparação
+             com outras rede.`) 
+
 
