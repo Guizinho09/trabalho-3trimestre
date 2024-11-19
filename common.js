7c64e0c00ede4609fcc090877493eb0646f040be
Guizinho09 <131281898+Guizinho09@users.noreply.github.com>
2024-11-19 09:38:56 -0300

    Update common.js

--- common.js
+++ common.js
@@ -1,12 +1,21 @@
+import {getcCSS, tickconfig} from "./common.js"
 const getCSS = (variavel) => {
-    const bodyStyles = getComputedStyle(document.body)
-    return bodyStyles.getPropertyValue(variavel)
+    return getComputedStyle(document.body).getPropertyValue(variavel)
 }
-
-const tickConfig = {
-    family: getCSS('--font'),
+const tickcongig = {
+    color: getcCSS('--primary-color),
     size: 16,
-    color: getCSS('--primary-color')
+    family: getCSS('--font')
 }
-
-export {getCSS, tickConfig}
+function criarGrafico(data, layout){
+    const grafico = document.createElement ('div')
+    grafico.className = 'grafico'
+    document.getElementByd('graficos-container')
+    .appendchild(grafico)
+    Plotly.newPlot(grafico,data, layout)
+}
+function incluirTexto(texto){
+    const contaiter = document.getElementById
+    ('graficos-container');
+    const paragrafo = document.
+    paragrafo.classlist.add('grafico
