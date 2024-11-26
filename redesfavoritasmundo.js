0ccd0583aee420fdf4ed7ec3358a15e400e7a550
Guizinho09 <131281898+Guizinho09@users.noreply.github.com>
2024-11-26 09:58:53 -0300

    Atualizar o redesfavoritasmundo.js

--- redesfavoritasmundo.js
+++ redesfavoritasmundo.js
@@ -1,26 +1,45 @@
-import { getcCSS } from"./common";
-const layout = {
-  plot_bgcolor: getCSS('--bg-color'),
-  paper_bgcolor: getCSS('--bg-color),
-  height: 700,
-  title: {
-  }
+import { getCSS, criarGrafico, incluirTexto } from "./common.js"
 
-  }
-const grafico = document.createElement ('div')
-         grafico.className = 'grafico'
-         document.getElementByd('graficos-container')
-         .appendchild(grafico)
-         const config = {
-            responsive: true,
-            displayModeBar: false
+async function redesFavoritasMundo() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/redes-favoritas.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const redes = Object.keys(dados)
+    const valores = Object.values(dados)
+
+    const data = [
+        {
+            values: valores,
+            labels: redes,
+            type: 'pie',
+            textinfo: 'label+percent'
         }
-        Plotly.newPlot(grafico,data,layout,confg)
+    ]
 
-        criarGrafico(data,layout)
-        incluirTexto(` Embora o <span>Instagram</span> ocupe a quarta posição em termos 
-             de número total de usuários entre as redes sociais, destaca-se como 
-             a <span>preferida dos usuários</span>. supera até mesmo o <span>Facebook</span>, a plataforma
-             com mais usuários, sendo terceira opção mais apreciada pelos usuários.<br>Essa preferéncia
-             envidencia a forte conexão e apreço que as pessoas têm pelo Instagram em comparação
-             com outras rede.`) 
+    const layout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color'),
+        height: 700,
+        title: {
+            text: 'Redes sociais que os usuários mais gostam',
+            x: 0,
+            font: {
+                color: getCSS('--primary-color'),
+                family: getCSS('--font'),
+                size: 30
+            }
+        },
+        legend: {
+            font: {
+                color: getCSS('--primary-color'),
+                size: 16
+            }
+        }
+    }
+
+    criarGrafico(data, layout)
+
+    incluirTexto(`Embora o <span>Instagram</span> ocupe a quarta posição em termos de número total de usuários entre as redes sociais, destaca-se como a <span>preferida pelos usuários</span>. Supera até mesmo o <span>Facebook</span>, a plataforma com mais usuários, sendo a terceira opção mais apreciada pelos usuários. <br>Essa preferência evidencia a forte conexão e apreço que as pessoas têm pelo Instagram em comparação com outras redes sociais`)
+}
+
+redesFavoritasMundo()
